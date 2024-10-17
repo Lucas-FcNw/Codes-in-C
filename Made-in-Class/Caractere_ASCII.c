ddb0aef9a8a7ea28740da4738b728d165429c63b
Lucas Fernandes de Camargo <135182612+Lucas-FcNw@users.noreply.github.com>
2024-10-16 23:04:35 -0300

    Atualizar o Caractere_ASCII.c
    ASCII final pt_br.

--- Made-in-Class/Caractere_ASCII.c
+++ Made-in-Class/Caractere_ASCII.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 #include <string.h>
 
-void toUpperCase(char str[]) {
+void maiusculas(char str[]) {
     for (int i = 0; str[i] != '\0'; i++) {
         str[i] = toupper(str[i]);
     }
@@ -10,45 +10,41 @@ void toUpperCase(char str[]) {
 
 int main() {
     char frase[256];
-    int mais = 0, minus = 0, dig = 0, espec = 0;
+    int mai = 0, min = 0, dig = 0, esp = 0;
 
-    // Solicita ao usuário para inserir uma frase
     printf("Insira uma frase: ");
     fgets(frase, sizeof(frase), stdin);
 
-    // Remove a nova linha, se presente
-    size_t len = strlen(frase);
-    if (len > 0 && frase[len - 1] == '\n') {
-        frase[len - 1] = '\0';
+    size_t tam = strlen(frase);
+    if (tam > 0 && frase[tam - 1] == '\n') {
+        frase[tam - 1] = '\0';
     }
 
-    // Exibe cada caractere e seu valor ASCII
-    printf("\nCaractere e seu valor ASCII:\n");
+    printf("\nCaractere e valor ASCII:\n");
     for (int i = 0; frase[i] != '\0'; i++) {
         printf("'%c' -> %d\n", frase[i], frase[i]);
 
-        // Contagem de tipos de caracteres
         if (isupper(frase[i])) {
-            maius++;
+            mai++;
         } else if (islower(frase[i])) {
-            minus++;
+            min++;
         } else if (isdigit(frase[i])) {
             dig++;
         } else if (!isspace(frase[i])) {
-            espec++;
+            esp++;
         }
     }
 
-    // Exibe contagens
     printf("\nTotal de caracteres:\n");
-    printf("Maiúsculas: %d\n", mais);
-    printf("Minúsculas: %d\n", minus);
+    printf("Maiúsculas: %d\n", mai);
+    printf("Minúsculas: %d\n", min);
     printf("Dígitos: %d\n", dig);
-    printf("Especiais: %d\n", espec);
+    printf("Especiais: %d\n", esp);
 
-    // Converte e exibe a frase em maiúsculas
-    toUpperCase(frase);
+    maiusculas(frase);
     printf("\nFrase em maiúsculas: %s\n", frase);
 
     return 0;
 }
+
+/* Feito por Lucas Fernandes. Algum dia vem a versão cometada e em inglês. */
